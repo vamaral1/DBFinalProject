6bc4316ac2d75a1ead6dadab00dff14cab3287cb
vamaral1 <vamaral1@jhu.edu>
2015-12-20 12:45:33 -0500

    fixed procedure call error

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from flask import Flask, render_template, request, json
 import MySQLdb as db
-#from flask.ext.mysql import MySQL
+import pdb
 
 app = Flask(__name__)
-#mysql = MySQL()
-# MySQL configurations
-# app.config['MYSQL_DATABASE_USER'] = 'dbproject'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'edvictor'
-# app.config['MYSQL_DATABASE_DB'] = 'twitterdata'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
-# conn = mysql.connect()
-# cur = conn.cursor()
-conn = db.connect(host = 'localhost', user="dbproject", passwd="edvictor", db="twitterdata")
-cur = conn.cursor()
 
 @app.route("/")
 def main():
@@ -21,24 +10,23 @@ def main():
 
 @app.route("/tweetsHashtag",methods=['POST','GET'])
 def tweetsHashtag():
-	#try:
-	hashtag = request.form['inputName']
-	print type(hashtag)
-	#call stored procedure
-	cur.callproc('FindTweetsGivenHashTag', (hashtag))
-	print "I came 3 times"
-	# If the procedure is executed successfully, then we'll commit the changes and return the success message. 
-	data = cur.fetchall()
-	if len(data) is 0:
-	    conn.commit()
-	    return json.dumps({'message':'Query successful!'})
-	else:
-	    return json.dumps({'error':str(data[0])})
-	# except Exception as e:
-	# 	return json.dumps({'error':str(e)})
-	# finally:
-	# 	cur.close()
-	# 	conn.close()
+	try:
+		hashtag = request.form['inputName']
+		print hashtag
+		#call stored procedure
+		conn = db.connect(host = 'localhost', user="dbproject", passwd="edvictor", db="twitterdata")
+		cur = conn.cursor()
+		cur.callproc('FindTweetsGivenHashTag', (hashtag,))
+		# If the procedure is executed successfully, then we'll commit the changes and return the success message. 
+		data = cur.fetchall()
+		print type(data)
+		for row in data:
+			print row
+	except Exception as e:
+		return json.dumps({'error':str(e)})
+	finally:
+		cur.close()
+		conn.close()
 
 if __name__ == "__main__":
     app.run()
